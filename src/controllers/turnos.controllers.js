289c5c22a18a8fd6df2811d548061937aae5c673
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-07-28 02:53:20 -0300

    controller crear turno

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -1,11 +1,20 @@
 import Turno from "../models/turnos";
 
-export const obtenerProductos = async (req,res)=>{
+export const obtenerTurnos = async (req,res)=>{
     try {
         const turnos = await Turno.find()
         res.status(200).json(turnos)
     } catch (error) {
         console.log(error);
         res.status(404).json({mensaje:'error al buscar los turnos'})
+    }
+}
+export const crearTurno = async(req,res) =>{
+    try {
+       const nuevoTurno = new Turno(req.body)
+       await nuevoTurno.save()
+       res.status(201).json({mensaje:"Se creo un nuevo turno"})
+    } catch (error) {
+        res.status(400).json(error.message)
     }
 }
