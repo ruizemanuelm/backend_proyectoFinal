17ddbc674e48f5c27e072c97ab7df78ef3bbf3a2
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-07-29 19:51:05 -0300

    controller borrar turno

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -18,3 +18,18 @@ export const crearTurno = async(req,res) =>{
         res.status(400).json(error.message)
     }
 }
+export const borrarTurno = async (req, res) => {
+    try {
+      console.log(req.params.id);
+      await Turno.findByIdAndDelete(req.params.id);
+      res.status(200).json({
+        mensaje: "El turno fue eliminado correctamente",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "No se pudo eliminar el turno",
+      });
+    }
+  };
+  
--- src/routes/turnos.routes.js
+++ src/routes/turnos.routes.js
@@ -1,3 +1,8 @@
 import { Router } from "express";
+import { crearTurno, obtenerTurnos } from "../controllers/turnos.controllers";
 
 const router = Router()
+router.get("/turnos",obtenerTurnos)
+router.post("/turnos",crearTurno)
+
+export default router
