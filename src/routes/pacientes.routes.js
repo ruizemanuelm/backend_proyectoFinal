07ab2dd7b2061eca889c4190045c8a72d7c604d4
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-29 21:17:55 -0300

    agregue las validaciones check en pacientes routes y validationResult en el controlador de productos

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -6,10 +6,27 @@ import {
   obtenerPacientePorId,
   obtenerPacientes,
 } from "../controllers/pacientes.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
-router.route("/pacientes").get(obtenerPacientes).post(crearPacientes);
+router
+  .route("/pacientes")
+  .get(obtenerPacientes)
+  .post(
+    [
+      check("nombreDueno").notEmpty().withMessage("El nombre es obligatorio"),
+      check("apellidoDueno").notEmpty().withMessage("El apellido es obligatorio"),
+      check("email").notEmpty().isEmail().withMessage("El email es obligatorio"),
+      check("telefono").notEmpty().isMobilePhone("any",{
+        strictMode: false,
+      }).withMessage("El telefono es obligatorio"),
+      check("direccion").notEmpty().withMessage("La direccion es obligatoria"),
+      check("nombreMascota").notEmpty().withMessage("El nombre de la mascota es obligatorio"),
+      check("especie").notEmpty().withMessage("Debe aclarar la especie")
+    ],
+    crearPacientes
+  );
 router
   .route("/pacientes/:id")
   .delete(borrarPaciente)
