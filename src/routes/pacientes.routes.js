5418cf3b823e2baddbb73752545eda468af7c447
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-30 17:02:57 -0300

    validaciones

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -16,14 +16,44 @@ router
   .post(
     [
       check("nombreDueno").notEmpty().withMessage("El nombre es obligatorio"),
-      check("apellidoDueno").notEmpty().withMessage("El apellido es obligatorio"),
-      check("email").notEmpty().isEmail().withMessage("El email es obligatorio"),
-      check("telefono").notEmpty().isMobilePhone("any",{
-        strictMode: false,
-      }).withMessage("El telefono es obligatorio"),
-      check("direccion").notEmpty().withMessage("La direccion es obligatoria"),
-      check("nombreMascota").notEmpty().withMessage("El nombre de la mascota es obligatorio"),
-      check("especie").notEmpty().withMessage("Debe aclarar la especie")
+      check("apellidoDueno")
+        .notEmpty()
+        .withMessage("El apellido es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage("El nombre debe tener entre 2 y 100 caracteres"),
+      check("email")
+        .notEmpty()
+        .withMessage("El email es obligatorio")
+        .isEmail()
+        .withMessage("Debe tener un de email valido")
+        .isLength({ min: 2, max: 100 })
+        .withMessage("El email debe contener entre 2 y 100 caracteres"),
+      check("telefono")
+        .notEmpty()
+        .withMessage("El telefono es obligatorio")
+        .isMobilePhone("any", {
+          strictMode: false,
+        })
+        .withMessage("El telefono debe contener un formato valido")
+        .isLength({ min: 5, max: 20 })
+        .withMessage("El telefono debe contener entre 5 y 20 caracteres"),
+      check("direccion")
+        .notEmpty()
+        .withMessage("La direccion es obligatoria")
+        .isLength({ min: 2, max: 100 })
+        .withMessage("La direccion debe contener entre 2 y 100 caracteres"),
+      check("nombreMascota")
+        .notEmpty()
+        .withMessage("El nombre de la mascota es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+          "El nombre de la mascota puede contener entre 2 y 100 caracteres"
+        ),
+      check("especie")
+        .notEmpty()
+        .withMessage("Debe aclarar la especie")
+        .isIn(["Perro", "Gato", "Conejo"])
+        .withMessage("Debe elegir una de las siguientes opciones; Gato,Perro, Conejo"),
     ],
     crearPacientes
   );
