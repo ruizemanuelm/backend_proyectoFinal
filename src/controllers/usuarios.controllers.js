de7c88940cd62e069b04e8129b6a3ed975b0b271
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-08-05 21:37:01 -0300

    agregamos para en encriptar la password

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/usuarios";
+import bcrypt from 'bcrypt';
 
 export const loguearUsuario = async (req, res) => {
     try {
@@ -29,7 +30,14 @@ export const loguearUsuario = async (req, res) => {
 
 export const crearUsuario = async(req,res) =>{
     try { 
-        const nuevoUsuario = new Usuario(req.body)
+        let consultarUsuario = await Usuario.findOne({email: req.body.email});
+        if(consultarUsuario){
+            return res.status(400).json({mensaje: 'ya existe un usuario con el correo enviado'})
+        }
+
+        const nuevoUsuario = new Usuario(req.body);
+        const salt = bcrypt.genSaltSync(10)
+        nuevoUsuario.password = bcrypt.hashSync(nuevoUsuario.password, salt);
         await nuevoUsuario.save()
         return res.status(201).json({mensaje:"Se creo un nuevo usuario"})
     } catch (error) {
